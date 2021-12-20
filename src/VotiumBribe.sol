95e3e485af7af4f0e7a32f6b25e765771bf6cbd1
scamilcar <64386795+scamilcar@users.noreply.github.com>
2021-12-20 17:14:41 +0100

    Initial commit

--- src/VotiumBribe.sol
+++ src/VotiumBribe.sol
@@ -1,5 +1,73 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-contract VotiumBribe {
+interface IVotiumBribe {
+    function depositBribe(address _token, uint256 _amount, bytes32 _proposal, uint256 _choiceIndex) external;
+}
+
+interface IERC20 {
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+contract Incentive {
+    
+    address public VOTIUM_BRIBE = 0x19BBC3463Dd8d07f55438014b021Fb457EBD4595;
+    
+    address public incentiveToken;
+    address public targetGauge;
+    uint256 public amountPerVote;
+    uint256 public voteChoiceIndex;
+    
+    uint256 public activePeriod;
+    uint256 public WEEK = 86400 * 7;
+
+    // Emitted when a '_depositor' deposits '_amount' '_incentiveToken'.
+    event Deposited(address indexed _depositor, uint256 _amount);
+    // Emitted when a vote is incentivized.
+    event VoteIncentivized(bytes32 _proposal, uint256 _date);
+    
+    constructor (
+        address _incentiveToken,
+        uint256 _amountPerVote, 
+        address _targetGauge, 
+        uint256 _voteChoiceIndex
+        ) {
+        incentiveToken = _incentiveToken;
+        amountPerVote = _amountPerVote;
+        targetGauge = _targetGauge;
+        // Set the Convex vote choice index to ensure this contract can only incentivize targetGauge.
+        // After checking the IPFS URIs of past votes on vote.convexfinance.com,
+        // it seems that gauge choices always have the same indexes so it should do the work.
+        voteChoiceIndex = _voteChoiceIndex;
+    }
+
+    // Allows anyone to deposited a desired '_amount' of 'incentiveTokens' in this contract.
+    // Refunds the depositor 'refund' if 'amount' is not a multiple of 'amountPerVote'.
+    function depositIncentive(uint _amount) external returns (bool) {
+        require(_amount >= amountPerVote, "Not enough tokens");
+        uint256 refund = _amount % amountPerVote;
+        uint256 deposit = _amount - refund;
+        IERC20(incentiveToken).transferFrom(msg.sender, address(this), deposit);
+        emit Deposited(msg.sender, deposit);
+        return true;
+    }
+
+    // Deposits 'amountPerVote' of 'incentiveToken on Votium.
+    // Reverts if called before 2 weeks have passed since the last call,
+    // meaning that the vote has already been incentivized by this contract for this vote.
+    // Important : Should be called for the first time just after the next Convex Snapshot gauge weight vote goes live
+    // (Thursday 00:00 UTC every 2 weeks) so that it can only be called bi-weekly after that, thus only allowing 1 bribe of 'amountPerVote' per '_proposal'.
+    function incentivizeVote(bytes32 _proposal) external returns (bool) {
+        require(block.timestamp >= activePeriod + WEEK * 2, "Vote already incentivized.");
+        activePeriod = block.timestamp;
+        IERC20(incentiveToken).approve(VOTIUM_BRIBE, amountPerVote);
+        IVotiumBribe(VOTIUM_BRIBE).depositBribe(incentiveToken, amountPerVote, _proposal, voteChoiceIndex);
+        emit VoteIncentivized(_proposal, activePeriod);
+        return true;
+    }
+
+
+
 }
--- src/VotiumBribe.t.sol
+++ src/VotiumBribe.t.sol
@@ -1,22 +1,49 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.6;
 
-import "ds-test/test.sol";
-
+import "../lib/ds-test/src/test.sol";
 import "./VotiumBribe.sol";
 
 contract VotiumBribeTest is DSTest {
-    VotiumBribe bribe;
+
+    address constant FLX = 0x6243d8CEA23066d098a15582d81a598b4e8391F4;
+    uint256 constant AMOUNT_PER_VOTE = 50*10**18;
+    
+    Incentive incentive;
+    IERC20 flx;
 
     function setUp() public {
-        bribe = new VotiumBribe();
+        incentive = new Incentive(FLX, AMOUNT_PER_VOTE, address(0x1));
+        flx = IERC20(FLX);
+    }
+    
+    // Should deposit 'amount' in the contract.
+    function test_depositIncentive() public {
+        uint256 amount = 100*10**18;
+        uint256 preBalance = flx.balanceOf(address(incentive));
+        flx.approve(address(incentive), amount);
+        incentive.depositIncentive(amount);
+        uint256 postBalance = flx.balanceOf(address(incentive));
+        assertEq(preBalance + amount, postBalance);
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    // Should test if it is possible to deposit 'amount' in the contract and fail since depositing less than amountPerVote.
+    function testFail_depositIncentive() public {
+        uint256 amount = 40*10**18;
+        flx.approve(address(incentive), amount);
+        incentive.depositIncentive(amount);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
-    }
+    function test_incentivizeGauge() public {
+        uint256 amount = 100*10**18;
+        flx.approve(address(bribe), amount);
+        incentive.depositIncentive(amount);
+        uint256 preBalanceBribe = spell.balanceOf(address(bribe));
+        uint256 preBalanceCRVBribeV2 = spell.balanceOf(BRIBE_V2);
+        incentive.incentivizeVote();
+        uint256 postBalanceBribe = spell.balanceOf(address(bribe));
+        uint256 postBalanceCRVBribeV2 = spell.balanceOf(BRIBE_V2);
+        assertEq(preBalanceBribe - AMOUNT_PER_VOTE, postBalanceBribe);
+        assertEq(preBalanceCRVBribeV2 + AMOUNT_PER_VOTE, postBalanceCRVBribeV2);
+
 }
